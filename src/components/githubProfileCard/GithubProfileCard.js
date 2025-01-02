1064cd6d36166cca615ceb591ac449e008a2d10c
omeryasirkucuk <omeryasirkucuk@gmail.com>
2025-01-02 19:18:10 +0300

    updated and pushed

--- src/components/githubProfileCard/GithubProfileCard.js
+++ src/components/githubProfileCard/GithubProfileCard.js
@@ -14,13 +14,9 @@ export default function GithubProfileCard({prof}) {
   return (
     <Fade bottom duration={1000} distance="20px">
       <div className="main" id="contact">
-        <h1 className="prof-title">Reach Out to me!</h1>
+        <h1 className="prof-title">{contactInfo.title}</h1>
         <div className="row">
           <div className="main-content-profile">
-            <div className="blog-header">
-              <p className="subTitle blog-subtitle">{contactInfo.subtitle}</p>
-            </div>
-            <h2 className="bio-text">"{emoji(String(prof.bio))}"</h2>
             {prof.location !== null && (
               <div className="location-div">
                 <span className="desc-prof">
@@ -41,11 +37,8 @@ export default function GithubProfileCard({prof}) {
                 </span>
               </div>
             )}
-            <div className="opp-div">
-              <span className="desc-prof">
-                Open for opportunities: {prof.hireable}
-              </span>
-            </div>
+            <p className="bio-text">{contactInfo.subtitle}</p>
+            <p className="mail-text">{contactInfo.email_address}</p>
             <SocialMedia />
           </div>
           <div className="image-content-profile">
